9cb6edbc854795f6a7da246d39b6ff7f22222eb1
Daniel Marino-Johnson <daniel@symphony>
2025-10-07 13:59:26 -0400

    clean up product details page

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -25,21 +25,45 @@ function ProductDetails({ product_no }) {
   return (
     <div style={{ marginTop: '2em', textAlign: 'center', color: 'white' }}>
       <h1>{data.name}</h1>
-      <h2>by {data.artist}</h2>
-      <h2>{data.year}</h2>
-      <h2>${data.price}</h2>
       <div class="row">
 	<div class="column">
+	  <div style={{
+	       border: '1px solid #ccc',
+	       borderRadius: '8px',
+	       padding: '1em',
+	       textAlign: 'Left',
+	       backgroundColor: 'black'
+               }}>
+	    <p>
+	      by {data.artist}
+	    </p>
+	    <p>
+	      {data.year}
+	    </p>
+	    <p>
+	      ${data.price}
+	    </p>
+
+	    <h2>Notes</h2>
+	    <p>
+	      Omnis aperiam placeat consequatur. Inventore et et blanditiis aut et. Odio soluta deleniti aut sunt molestiae. Necessitatibus eum architecto qui. In veniam quibusdam sed cum fugit repudiandae dolor
+	    </p>
+
+	    <h2>Tracks</h2>
+	    
 	  <table>
 	    <tbody>
               {tracks.map((track, idx) => (
 		<tr key={idx}>
-		  <td>{track.track_number}</td>
+		  <td>
+		    <div style={{marginRight: '1em'}} >{track.track_number}</div>
+		  </td>
 		  <td>{track.track_name}</td>
 		</tr>
               ))}
 	    </tbody>
 	  </table>
+	  </div>
 	</div>
 	<div class="column">
 	  <img src={image} alt="album" style={{ width: '50%', borderRadius: '4px' }} />
